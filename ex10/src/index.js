42a2e9a43fc4f6977134740adf48ec9c530415bd
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-12 00:56:54 +0100

    my name is Bond

---
+++ ex10/src/index.js
@@ -0,0 +1,17 @@
+function main (){
+
+    // only change code below this line
+    
+    var myFirstName = "Kenan"
+    var myLastName = "Serhatlić"
+    var myHomeTown = "Priboj"
+
+    // Only change code above this line
+    
+    return {myFirstName,
+    myLastName,
+    myHomeTown};
+}
+
+console.log(main()); // change this line
+module.exports = main;
