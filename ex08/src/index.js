f72c5a3eb671d600cfa28b05b3e5178c6d020131
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-12 00:44:00 +0100

    18 to 19

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -3,7 +3,7 @@ function main (){
     // only change code below this line
     
     var myX = 12; myX += 3
-    var myY = 18; myY += 17
+    var myY = 19; myY += 17
     var myZ = 24; myZ -= 20
 
     // Only change code above this line
