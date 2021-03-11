25e23ce2abcd248ae22df9d8ad56b38375d819c3
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-12 00:43:05 +0100

    myZ with myX

---
+++ ex08/src/index.js
@@ -0,0 +1,17 @@
+function main (){
+
+    // only change code below this line
+    
+    var myX = 12; myX += 3
+    var myY = 18; myY += 17
+    var myZ = 24; myZ -= 20
+
+    // Only change code above this line
+    
+    return {myX,
+    myY,
+    myZ};
+}
+
+console.log(main()); // change this line
+module.exports = main;
