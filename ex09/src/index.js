ca0cc56630dad65614a19ffe1d84228af0c86340
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-12 00:51:36 +0100

    again little mist fixed

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -3,7 +3,7 @@ function main (){
     // only change code below this line
     
     var firstNum = 6; firstNum *= 9
-    var secondNum = 25; myY /= 5
+    var secondNum = 25; secondNum /= 5
     var thirdNum = 5.2; thirdNum *= 10
 
     // Only change code above this line
