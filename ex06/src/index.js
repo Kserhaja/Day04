921ecaf5805dfc73a30d804d0c7c0aeab763fdb0
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-12 00:28:53 +0100

    quotient finnished

---
+++ ex06/src/index.js
@@ -0,0 +1,17 @@
+function main (numOne, numTwo){
+
+    // only change code below this line
+    
+    var numOne;
+    var numTwo;
+    var myQuotient = numOne / numTwo
+
+    // Only change code above this line
+    
+    return myQuotient;
+}
+
+console.log(main(5, 2));
+console.log(main(16.5, 5.5));
+console.log(main(102.00, 25.50));
+module.exports = main;
