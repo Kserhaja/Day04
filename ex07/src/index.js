f9036c488eef8a54ca238326954d4d9b6f2753b9
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-12 00:34:10 +0100

    remind reminder

---
+++ ex07/src/index.js
@@ -0,0 +1,17 @@
+function main (numOne, numTwo){
+
+    // only change code below this line
+    
+    var numOne;
+    var numTwo;
+    var reminder = numOne % numTwo
+
+    // Only change code above this line
+    
+    return reminder;
+}
+
+console.log(main(5, 2)); // change this line
+console.log(main(16.5, 5.5)); // change this line
+console.log(main(102.00, 25.50)); // change this line
+module.exports = main;
